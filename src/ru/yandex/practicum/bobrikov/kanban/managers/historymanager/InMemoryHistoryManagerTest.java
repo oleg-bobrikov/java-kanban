92c8744073bffe768934bcea73b282c0eb457f37
Oleg Bobrikov <bobrikov@yandex.ru>
2022-12-04 20:37:59 +0300

    fix: InMemoryTaskManagerTest (update add() test)

--- src/ru/yandex/practicum/bobrikov/kanban/managers/historymanager/InMemoryHistoryManagerTest.java
+++ src/ru/yandex/practicum/bobrikov/kanban/managers/historymanager/InMemoryHistoryManagerTest.java
@@ -12,19 +12,19 @@ class InMemoryHistoryManagerTest {
 
     @Test
     void add() {
-        ArrayList<Integer> taskIdentifiers = new ArrayList<>();
-        for (int i = 1; i <= 11; i++) {
-            Task task = createTask(i);
-            taskIdentifiers.add(task.getId());
-        }
+        taskManager.deleteTasks();
+        Task task1 = createTask(1);
+        Task task2 = createTask(2);
+
         // Просматриваем задачи
-        for (Integer taskId : taskIdentifiers) {
-            taskManager.getTask(taskId);
-        }
-        // Ожидаем в списке просмотренных задач ровно 10 вместо 11
-        // и первая задача должна затереться второй
+        taskManager.getTask(task1.getId());
+        taskManager.getTask(task2.getId());
+        taskManager.getTask(task1.getId());
+
+        // Все должно быть 2 задачи в списке просмотра
+        // и первая задача должна быть с идентификтором 2
         ArrayList<Task> history = taskManager.getHistory();
-        assert history.size() == 10 && history.get(0).getName().contains("Задача: 2");
+        assert history.size() == 2 && history.get(0).getName().contains("Задача: 2");
     }
 
     private Task createTask(int taskNumber) {
