7c7480f83a5dc5cf213f1bc5bb72984cfe2fdd18
Oleg Bobrikov <bobrikov@yandex.ru>
2023-01-02 20:54:28 +0300

    refactor: public static FileBackedTaskManager::Main(String args[]) fix: ManagerSaveException references (only unchecked) fix: remove checked ManagerSaveException from the Main class signature

--- src/main/java/ru/yandex/practicum/bobrikov/kanban/managers/taskmanager/FileBackedTaskManager.java
+++ src/main/java/ru/yandex/practicum/bobrikov/kanban/managers/taskmanager/FileBackedTaskManager.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.bobrikov.kanban.managers.taskmanager;
 
 import ru.yandex.practicum.bobrikov.kanban.exceptions.ManagerSaveException;
+import ru.yandex.practicum.bobrikov.kanban.managers.Managers;
 import ru.yandex.practicum.bobrikov.kanban.managers.historymanager.HistoryManager;
 import ru.yandex.practicum.bobrikov.kanban.model.Epic;
 import ru.yandex.practicum.bobrikov.kanban.model.SubTask;
@@ -8,13 +9,14 @@ import ru.yandex.practicum.bobrikov.kanban.model.Task;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     private final File file;
 
-
-    public FileBackedTaskManager(HistoryManager historyManager, File file)  {
+    public FileBackedTaskManager(HistoryManager historyManager, File file) {
         super(historyManager);
         this.file = file;
         loadFromFile();
@@ -161,4 +163,80 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         save();
         return subTask;
     }
+
+
+    public static void main(String[] args) {
+        File taskManagerFile = new File("taskManager.txt");
+        try {
+            Files.deleteIfExists(taskManagerFile.toPath());
+        } catch (IOException e) {
+            throw new ManagerSaveException(e.getMessage());
+        }
+
+        File newTaskManagerFile = new File("taskManager_copy.txt");
+
+        // Тестовый сценарий №1
+        // Заведите несколько разных задач, эпиков и подзадач.
+        InMemoryTaskManager taskManager = Managers.getFileBackedTaskManager(taskManagerFile);
+        createTasks(taskManager);
+
+        // Тестовый сценарий №2
+        // Запросите некоторые из них, чтобы заполнилась история просмотра.
+
+        Task task1 = taskManager.getTask(1);// История просмотра:{1}
+        Task task2 = taskManager.getTask(2);// История просмотра:{1, 2}
+        Epic epic3 = taskManager.getEpic(3);// История просмотра:{1, 2, 3}
+        SubTask subTask4 = taskManager.getSubTask(4);// История просмотра:{1, 2, 3, 4}
+        SubTask subTask5 = taskManager.getSubTask(5);// История просмотра:{1, 2, 3, 4, 5}
+        SubTask subTask6 = taskManager.getSubTask(6);// История просмотра:{1, 2, 3, 4, 5, 6}
+        taskManager.getSubTask(4);// Задачи:{1, 2, 3, 5, 6, 4}
+        taskManager.getTask(1);// Задачи:{2, 3, 5, 6, 4, 1}
+        printHistory(taskManager);
+
+        // Тестовый сценарий №3
+        // Создайте новый FileBackedTasksManager менеджер из этого же файла.
+        try {
+            Files.copy(taskManagerFile.toPath(), Path.of("taskManager_copy.txt"), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new ManagerSaveException(e.getMessage());
+        }
+        InMemoryTaskManager newTaskManager = Managers.getFileBackedTaskManager(newTaskManagerFile);
+        printHistory(newTaskManager);
+
+        boolean passed = taskManager.equals(newTaskManager);
+        System.out.println(passed ? "Test has passed." : "Test has failed.");
+
+    }
+
+    private static void printHistory(TaskManager taskManager) {
+        System.out.println("Список просмотренных задач:");
+        for (Task task : taskManager.getHistory()) {
+            System.out.println(task);
+        }
+        System.out.println();
+    }
+
+    private static void createTasks(TaskManager taskManager) {
+        Task task = new Task("Сварить борщ", "на воде, без мяса, добавить чеснока, приготовиь сало");
+        taskManager.addTask(task);
+
+        task = new Task("Сделать уборку квартиры", "Обязательно вымыть зеркало в ванной");
+        taskManager.addTask(task);
+
+        Epic epic = new Epic("Переобуть машину", "Kia Rio с летней на зимнюю резину");
+        taskManager.addEpic(epic);
+
+        SubTask subTask = new SubTask("Купить шины", "Michelin 185x65 R15", epic);
+        taskManager.addSubTask(subTask);
+
+        subTask = new SubTask("Записаться на шиномонтаж", "Можно в Колобокс или Колесо", epic);
+        taskManager.addSubTask(subTask);
+
+        subTask = new SubTask("Продать старые шины", "Gislaved 200 185x65 R15", epic);
+        taskManager.addSubTask(subTask);
+
+        epic = new Epic("Подготовитсья к экзамену по английскому языку", "SkyEng");
+        taskManager.addEpic(epic);
+    }
 }
+
--- src/test/java/ru/yandex/practicum/bobrikov/kanban/FileBackedTaskManager.java
+++ src/test/java/ru/yandex/practicum/bobrikov/kanban/FileBackedTaskManager.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.bobrikov.kanban;
 
-import ru.yandex.practicum.bobrikov.kanban.exceptions.ManagerSaveException;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.bobrikov.kanban.managers.Managers;
 import ru.yandex.practicum.bobrikov.kanban.managers.taskmanager.InMemoryTaskManager;
+import ru.yandex.practicum.bobrikov.kanban.managers.taskmanager.TaskManager;
 import ru.yandex.practicum.bobrikov.kanban.model.Epic;
 import ru.yandex.practicum.bobrikov.kanban.model.SubTask;
 import ru.yandex.practicum.bobrikov.kanban.model.Task;
-import ru.yandex.practicum.bobrikov.kanban.managers.Managers;
-import ru.yandex.practicum.bobrikov.kanban.managers.taskmanager.TaskManager;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,10 +14,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-public class Main {
-    public static void main(String[] args) throws ManagerSaveException, IOException {
+class MainTest {
+
+    @Test
+    void main() {
         File taskManagerFile = new File("taskManager.txt");
-        Files.deleteIfExists(taskManagerFile.toPath());
+        try {
+            Files.deleteIfExists(taskManagerFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         File newTaskManagerFile = new File("taskManager_copy.txt");
 
@@ -37,28 +43,23 @@ public class Main {
         SubTask subTask6 = taskManager.getSubTask(6);// История просмотра:{1, 2, 3, 4, 5, 6}
         taskManager.getSubTask(4);// Задачи:{1, 2, 3, 5, 6, 4}
         taskManager.getTask(1);// Задачи:{2, 3, 5, 6, 4, 1}
-        printHistory(taskManager);
+
 
         // Тестовый сценарий №3
         // Создайте новый FileBackedTasksManager менеджер из этого же файла.
-        Files.copy(taskManagerFile.toPath(), Path.of("taskManager_copy.txt"), StandardCopyOption.REPLACE_EXISTING);
-        InMemoryTaskManager newTaskManager = Managers.getFileBackedTaskManager(newTaskManagerFile);
-        printHistory(newTaskManager);
-
-        boolean passed = taskManager.equals(newTaskManager);
-        System.out.println(passed ? "Test has passed." : "Test has failed.");
-
-    }
-
-    private static void printHistory(TaskManager taskManager) {
-        System.out.println("Список просмотренных задач:");
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
+        try {
+            Files.copy(taskManagerFile.toPath(), Path.of("taskManager_copy.txt"), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println();
+        InMemoryTaskManager newTaskManager = Managers.getFileBackedTaskManager(newTaskManagerFile);
+
+
+        assert taskManager.equals(newTaskManager);
+
     }
 
-    private static void createTasks(TaskManager taskManager) throws ManagerSaveException {
+    private static void createTasks(TaskManager taskManager) {
         Task task = new Task("Сварить борщ", "на воде, без мяса, добавить чеснока, приготовиь сало");
         taskManager.addTask(task);
 
